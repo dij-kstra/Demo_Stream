d9aa6be99d2d84f3597a100eb4d348c11c31396b
dij-kstra <140788057+dij-kstra@users.noreply.github.com>
2023-07-30 22:19:25 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -18,7 +18,7 @@ import streamlit as st
 #Other imports
 import pandas as pd
 import numpy as np
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 # from sklearn.linear_model import LogisticRegression
 # import matplotlib.pyplot as plt
 # from sklearn.metrics import accuracy_score
