e35e45b7d1d62d0fe2709c4373a6287c66fd39f1
dij-kstra <140788057+dij-kstra@users.noreply.github.com>
2023-07-30 22:54:49 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,13 +3,13 @@ import streamlit as st
 #Other imports
 import pandas as pd
 import numpy as np
-#from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
-#from sklearn.metrics import accuracy_score
-# from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
-# Title of the dashboard
+Title of the dashboard
 st.title("hii dear, I have started")
 
 st.write("You can talk now")
@@ -18,38 +18,38 @@ st.write("You can talk now")
 st.write("Upload your dataset (CSV file format)")
 file = st.file_uploader("", type="csv")
 
-#read the csv file and display the dataframe
-# if file is not None:
-#     data = pd.read_csv(file)
-#     st.write("Preview of the uploaded dataset:")
-#     st.dataframe(data)
+read the csv file and display the dataframe
+if file is not None:
+    data = pd.read_csv(file)
+    st.write("Preview of the uploaded dataset:")
+    st.dataframe(data)
 
-#     target = st.selectbox('Select the target variable: ', 
-#     list(data.columns), index = list(data.columns).index(list(data.columns)[-1]))
-#     X = data.drop(columns=target)
-#     y = data[target]
+    target = st.selectbox('Select the target variable: ', 
+    list(data.columns), index = list(data.columns).index(list(data.columns)[-1]))
+    X = data.drop(columns=target)
+    y = data[target]
 
-# # split the dataset into train and test and traina  logistic regrresison model
-#     st.write("Splitting the dataset into training and testing sets:")
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
+# split the dataset into train and test and traina  logistic regrresison model
+    st.write("Splitting the dataset into training and testing sets:")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
     
-#     random_state=0)
+    random_state=0)
 
-#     sc = StandardScaler()
-#     X_train = sc.fit_transform(X_train)
-#     X_test = sc.transform(X_test)
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
 
-#     st.write("Training a Logistic Regression Model:")
-#     model = LogisticRegression(random_state = 0, solver='lbfgs', multi_class='auto')
-#     model.fit(X_train, y_train)
+    st.write("Training a Logistic Regression Model:")
+    model = LogisticRegression(random_state = 0, solver='lbfgs', multi_class='auto')
+    model.fit(X_train, y_train)
 
-# #Evaluate the model and print the accuracy score
-#     st.write("Evaluating the Model:")
-#     y_pred = model.predict(X_test)
-#     accuracy = accuracy_score(y_test, y_pred)
-#     st.write("Accuracy: ", accuracy)
+#Evaluate the model and print the accuracy score
+    st.write("Evaluating the Model:")
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    st.write("Accuracy: ", accuracy)
 
-# st.write("End of Training")
-# I
+st.write("End of Training")
+I
 
