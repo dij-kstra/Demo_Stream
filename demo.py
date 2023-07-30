2363e55fb3cb561b33ef6c4ed7b09a1f8a40d283
dij-kstra <140788057+dij-kstra@users.noreply.github.com>
2023-07-30 23:11:09 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
-Title of the dashboard
+#Title of the dashboard
 st.title("hii dear, I have started")
 
 st.write("You can talk now")
